bfa1b97f4d0f01a0a3bd85ba7f265ac5d4b5f370
Shreyas Thale <155833376+ShreyasT123@users.noreply.github.com>
2025-03-01 10:35:23 +0530

    Update index.js

--- helper/index.js
+++ helper/index.js
@@ -9,13 +9,12 @@ import { collection, addDoc, getDocs, query } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBz-v5Ji_ph5u-ua8RJW9_DivmpMRxKPHA",
-  authDomain: "nextfire-bc74f.firebaseapp.com",
-  projectId: "nextfire-bc74f",
-  storageBucket: "nextfire-bc74f.firebasestorage.app",
-  messagingSenderId: "484106134711",
-  appId: "1:484106134711:web:ffc2720fe0a07991e85932",
-  measurementId: "G-Z6QEE5K9D1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
